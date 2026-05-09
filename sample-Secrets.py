9be952bc7fb1c1ce9330496b09db5d680d610e44
hsneed5 <hsneed5@ivytech.edu>
2026-05-09 16:01:01 -0400

    Create sample-Secrets.py

---
+++ sample-Secrets.py
@@ -0,0 +1,21 @@
+# Example hardcoded secrets for testing the scanner
+
+AWS_ACCESS_KEY = "AKIAIOSFODNN7EXAMPLE"
+
+google_api_key = "AIzaSyAExampleKey1234567890123456789"
+
+password = "SuperSecretPassword123!"
+
+github_token = "ghp_abcdefghijklmnopqrstuvwxyz123456"
+
+jwt_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiYWRtaW4ifQ.signature"
+
+slack_token = "xoxb-123456789012-123456789012-abcdefghijklmnop"
+
+stripe_secret = "sk_live_1234567890abcdefghijklmn"
+
+private_key = '''
+-----BEGIN RSA PRIVATE KEY-----
+MIIEowIBAAKCAQEAuExampleFakePrivateKeyDataOnlyForTesting
+-----END RSA PRIVATE KEY-----
+'''
